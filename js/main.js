6b5a255d66f6b2603cd989c0dff4b9bc3c78b01c
sigurdstuge <sigurd.stuge@gmail.com>
2022-11-22 16:04:50 +0100

    imported js files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import header from './modules/contact.js';
+import contact from './modules/header.js';
+import series from './modules/series.js';
+import slideshow from './modules/slideshow';
+
+
+contact();
+header();
+series();
+slideshow();
