f48ea70503c4aab6789fa2c76a797f2488728623
sigurdstuge <sigurd.stuge@gmail.com>
2022-11-24 11:31:11 +0100

    made menu functional

--- js/modules/header.js
+++ js/modules/header.js
@@ -1,3 +1,50 @@
 export default function header() {
+	// Data
+	let isNavigationOpen = false;
+
+	// Querry selectors
+	const navigationButton = document.querySelector('.header__navigation-button')
+	const navigation = document.querySelector('.header__navigation-list')
+	const navigationButtonSVG = document.querySelector('.header__navigation-button-svg')
+
+	// Event Listener
+	navigationButton.addEventListener('click', handleNavigationButtonClick)
 	
+	// Handlers 
+	function handleNavigationButtonClick() {
+		toggleNavigation();
+		renderHTML();
+	}
+
+	// Methods
+	function toggleNavigation() {
+		if (isNavigationOpen === true) {
+			isNavigationOpen = false
+		} else {
+			isNavigationOpen = true;
+		}
+	}
+
+	function renderHTML() {
+		renderNavigation();
+		renderNavigationButton();
+	}
+
+	// add and remove class
+	function renderNavigation() {
+		if (isNavigationOpen === true) {
+			navigation.classList.add('header__navigation-list--visible');
+		} else {
+			navigation.classList.remove('header__navigation-list--visible');
+		}
+	}
+
+	// Change src on the button
+	function renderNavigationButton() {
+		if (isNavigationOpen === true) {
+			navigationButtonSVG.src='./assets/svg/close.svg'
+		} else {
+			navigationButtonSVG.src='./assets/svg/menu.svg'
+		}
+	}
 }
