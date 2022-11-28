270b87427983cc803462b7b3abf4cd8662ed4d1c
sigurdstuge <sigurd.stuge@gmail.com>
2022-11-28 11:03:58 +0100

    made slideshow functional

--- js/modules/slideshow.js
+++ js/modules/slideshow.js
@@ -1,16 +1,60 @@
 export default function slideshow() {
+	//data
+	let currentSlideIndex = 0;
+	
+	//Querryselector
 	const slides = document.querySelectorAll('.slideshow__slide');
 	const buttonLeft = document.querySelector('.slideshow__button--left');
 	const buttonRight = document.querySelector('.slideshow__button--right');
 	
-
+	//event listener
 	buttonLeft.addEventListener('click', handleButtonLeftClick);
+	buttonRight.addEventListener('click', handleButtonRightClick);
+
+	//handler
 	function handleButtonLeftClick() {
-		console.log('left')
+		decreaseCurrentSlideIndex();
+		renderHTML();
+	}
+	function handleButtonRightClick() {
+		increaseCurrentSlideIndex();
+		renderHTML();
+	}
+	
+	//methoods
+	function increaseCurrentSlideIndex() {
+		if (currentSlideIndex === slides.length - 1) {
+			currentSlideIndex = 0
+		} else {
+			currentSlideIndex += 1
+		}
+
+		console.log(currentSlideIndex);
 	}
 
-	buttonRight.addEventListener('click', handleButtonRightClick);
-	function handleButtonRightClick() {
-		console.log('right')
+	function decreaseCurrentSlideIndex() {
+		if (currentSlideIndex === 0) {
+			currentSlideIndex = slides.length - 1
+		} else {
+			currentSlideIndex -= 1
+		}
+		console.log(currentSlideIndex);
 	}
+
+	function renderHTML() {
+		renderSlideshow();
+	}
+
+	function renderSlideshow() {
+		for(let index = 0; index < slides.length; index += 1) {
+			slides[index].classList.remove('slideshow__slide--visible')
+		}
+
+
+		slides[currentSlideIndex].classList.add('slideshow__slide--visible')
+	}
+
+	// Called methods
+	renderHTML();
 }
+
